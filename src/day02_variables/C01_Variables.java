f0efe1d2ffa7361bf2127f4868199d6cedb18b78
samet <sametguler210@gmail.com>
2022-12-12 21:28:10 +0300

    12.12.2022

---
+++ src/day02_variables/C01_Variables.java
@@ -0,0 +1,20 @@
+package day02_variables;
+
+public class C01_Variables {
+    public static void main(String[] args) {
+
+        int not = 60;
+
+        int not2=70;
+
+        // not2'yi 80 yapalım
+
+        not2=80;
+
+        // yeni bir int variable oluşturalım, değeri ilk iki variable'ın ortalaması olsun
+
+        int ort1= (not+not2)/2;
+
+        System.out.println(ort1);
+    }
+}
---
+++ src/day02_variables/C02_Variables.java
@@ -0,0 +1,39 @@
+package day02_variables;
+
+public class C02_Variables {
+    public static void main(String[] args) {
+
+        int sayi=10;
+        boolean guzelMi=true;
+        char sayim=3;
+
+        String isim="mehmet";
+        String soyisim="bulutluoz";
+
+        System.out.println("isminiz : " + isim);
+        System.out.println("soyisminiz : " + soyisim);
+
+        int tamSayi=37;
+        int tamSayi2=63;
+
+        System.out.println(tamSayi+tamSayi2);
+
+        int tamSayi3=78;
+        double ondalikliSayi= 55.458675;
+
+        System.out.println(tamSayi3+ondalikliSayi);
+
+        char tekKarakter='*';
+
+        System.out.println(tekKarakter);
+
+        char yuzde='%';
+        int tamSayi4= 5;
+
+
+        System.out.println(yuzde+" "+tamSayi4);
+
+
+
+    }
+}
